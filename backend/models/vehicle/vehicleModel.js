06cb823f349a2b0a8499c3a20f145cda68feb477
VinuriKariyawasam <vinurikariyawasam8@gmail.com>
2024-03-14 01:43:27 +0530

    Update vehicleModel.js

--- backend/models/vehicle/vehicleModel.js
+++ backend/models/vehicle/vehicleModel.js
@@ -1,8 +1,44 @@
-class HttpError extends Error {
-    constructor(message, errorCode) {
-      super(message); // Add a "message" property
-      this.code = errorCode; // Adds a "code" property
-    }
-  }
-  
-  module.exports = HttpError;
+const mongoose = require("mongoose");
+
+const vehicleSchema = new mongoose.Schema({
+  vehicleNo: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  documents: [
+    {
+      type: String, // Array of file paths or references
+    },
+  ],
+  password: {
+    type: String,
+    required: function () {
+      // Require password field only for supervisor positions
+      this.position === "Supervisor";
+    },
+  },
+});
+
+const Vehicle = mongoose.model("Vehicle", vehicleSchema);
+
+module.exports = Vehicle;
