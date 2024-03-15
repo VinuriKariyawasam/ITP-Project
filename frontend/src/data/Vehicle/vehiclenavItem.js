ec84496ff05d994b66be7b0270f11f125355e31e
VinuriKariyawasam <vinurikariyawasam8@gmail.com>
2024-03-15 11:26:40 +0530

    Create vehiclenavItem.js

---
+++ frontend/src/data/Vehicle/vehiclenavItem.js
@@ -0,0 +1,39 @@
+const navList = [
+  {
+    _id: 1,
+    name: "Dashboard",
+    icon: "bi bi-grid",
+  },
+  {
+    _id: 2,
+    name: "Employee",
+    icon: "bi bi-people-fill",
+  },
+  {
+    _id: 3,
+    name: "Salary",
+    icon: "bi bi-cash",
+  },
+  {
+    _id: 4,
+    name: "Leaves",
+    icon: "bi bi-layout-text-window-reverse",
+  },
+  {
+    _id: 5,
+    name: "Attendance",
+    icon: "bi bi-card-checklist",
+  },
+  {
+    _id: 6,
+    name: "Reports",
+    icon: "bi bi-clipboard2-data",
+    children: [
+      { _id: 21, name: "Salary", icon: "bi bi-circle" },
+      { _id: 22, name: "Employee", icon: "bi bi-circle" },
+      { _id: 23, name: "Attendance", icon: "bi bi-circle" },
+    ],
+  },
+];
+
+export default navList;
