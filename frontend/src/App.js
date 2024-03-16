da536b292cb35f543af860c01584aadfe78199b4
VinuriKariyawasam <vinurikariyawasam8@gmail.com>
2024-03-16 18:03:57 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,15 @@
-//import icons
+// Import icons and Bootstrap
 import "bootstrap-icons/font/bootstrap-icons.css";
-//import "remixicon/fonts/remixicon.css";
-
-// Import Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-//Import front end routes
+// Import React and React Router
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
-  Redirect,
-  Switch,
+  Routes,
+  Navigate,
 } from "react-router-dom";
 
 import "./App.css";
@@ -24,11 +22,10 @@ function App() {
     <>
       <Header />
       <Router>
-        <Switch>
-          <Route path="/vehicle" component={Vehicle} />
-          <Route path="/" component={Common} />
-          <Redirect to="/" />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Common />} />
+          <Route path="/vehicle/*" element={<Vehicle />} />
+        </Routes>
       </Router>
     </>
   );
