a4c2e595298f83124e0f1173599c042c14b71ff5
VinuriKariyawasam <vinurikariyawasam8@gmail.com>
2024-03-16 21:25:48 +0530

    Create SuperVehicle.js

---
+++ frontend/src/components/SUPER/SuperMain/SuperVehicle.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./SuperMain.css";
+import SuperPageTitle from "./SUPERPageTitle";
+import AddVehicle from "./AddVehicle";
+import VehicleDash from "./VehicleDash";
+
+// Import front end routes
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+function SuperEmployee() {
+  return (
+    <main id="main" className="main">
+      <SuperPageTitle title="Vehicle" url="super/vehicle" />
+
+      <Routes>
+        <Route path="/" element={<VehicleDash />} />
+        <Route path="add" element={<AddVehicle />} />
+      </Routes>
+    </main>
+  );
+}
+
+export default SuperVehicle;
