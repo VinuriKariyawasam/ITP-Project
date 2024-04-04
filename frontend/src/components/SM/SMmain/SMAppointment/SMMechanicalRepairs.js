98bc9658e037a03356a56361be30ef07f52f26fe
IT22122032 <159409471+IT22122032@users.noreply.github.com>
2024-04-04 22:10:01 +0530

    Latest update

--- frontend/src/components/SM/SMmain/SMAppointment/SMMechanicalRepairs.js
+++ frontend/src/components/SM/SMmain/SMAppointment/SMMechanicalRepairs.js
@@ -6,7 +6,7 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 
 
-const SMMechanicalRepairs= props => {
+const SMMechanicalRepairs = props => {
 
   //create an empty array to store details
   const [mechanicalAppointment, setmechanicalAppointment] = useState([]);
@@ -16,11 +16,11 @@ const SMMechanicalRepairs= props => {
   const [vType, setvType] = useState("");
   const [vNo, setvNo] = useState("");
   const [serviceType, setserviceType] = useState("");
-  const [issue,setissue]=useState("");
+  const [issue, setissue] = useState("");
   const [contactNo, setcontactNo] = useState("");
   const [appointmentdate, setappointmentdate] = useState("");
   const [appointmenttime, setappointmenttime] = useState("");
-  
+
 
   function sendata(e) {
     e.preventDefault();
@@ -35,11 +35,11 @@ const SMMechanicalRepairs= props => {
       contactNo,
       appointmentdate,
       appointmenttime,
-      
+
     }
 
     axios.post("http://localhost:5000/appointment/addacceptedappointment", newacceptedappointment).then(() => {
-      alert("Your Appointment Success")  
+      alert("Your Appointment Success")
       Delete(selectedAppointment._id);
 
     }).catch((err) => {
@@ -102,34 +102,34 @@ const SMMechanicalRepairs= props => {
         <h2 className="SMAppheading">Mechanical Services</h2>
         {selectedAppointment && (
           <div className="SmCard">
-          <Card style={{width:"50%"}}>
-            <Card.Body>
-            <button type="button" class="btn-close" aria-label="Close"  onClick={handleCardClose}></button>
-              <Card.Title>Selected Appointment Details</Card.Title>
-              <Row>
-              <Card.Text  >
-                <strong >Vehicle No: </strong>{selectedAppointment.vNo}<br />
-                <strong >Customer Name: </strong>{selectedAppointment.name}<br />
-                </Card.Text>
+            <Card style={{ width: "50%" }}>
+              <Card.Body>
+                <button type="button" class="btn-close" aria-label="Close" onClick={handleCardClose}></button>
+                <Card.Title>Selected Appointment Details</Card.Title>
+                <Row>
+                  <Card.Text  >
+                    <strong >Vehicle No: </strong>{selectedAppointment.vNo}<br />
+                    <strong >Customer Name: </strong>{selectedAppointment.name}<br />
+                  </Card.Text>
                 </Row>
                 <Row>
-                <Card.Text>
-                <strong>Vehicle Type: </strong>{selectedAppointment.vType}<br />
-                <strong>Requesting service: </strong>{selectedAppointment.issue}<br />
-                </Card.Text>
+                  <Card.Text>
+                    <strong>Vehicle Type: </strong>{selectedAppointment.vType}<br />
+                    <strong>Requesting service: </strong>{selectedAppointment.issue}<br />
+                  </Card.Text>
                 </Row>
                 <Row>
-                <Card.Text>
-                <strong>Date and Time: </strong>{selectedAppointment.appointmentdate ? `${selectedAppointment.appointmentdate.split('T')[0]} ${selectedAppointment.appointmenttime}` : ''}<br />
-                <strong>Contact No: </strong>{selectedAppointment.contactNo}<br />
-                </Card.Text>
+                  <Card.Text>
+                    <strong>Date and Time: </strong>{selectedAppointment.appointmentdate ? `${selectedAppointment.appointmentdate.split('T')[0]} ${selectedAppointment.appointmenttime}` : ''}<br />
+                    <strong>Contact No: </strong>{selectedAppointment.contactNo}<br />
+                  </Card.Text>
                 </Row >
-                <Row style={{marginTop:"4%", display:"flex"}}>
-                <Button variant="danger" onClick={() => Delete(selectedAppointment._id)} style={{marginLeft:"20%",width:"100px"}}>Cancel</Button>
-                <Button variant="primary" onClick={sendata} style={{marginLeft:"20%",width:"100px"}}>Approve</Button>
+                <Row style={{ marginTop: "4%", display: "flex" }}>
+                  <Button variant="danger" onClick={() => Delete(selectedAppointment._id)} style={{ marginLeft: "20%", width: "100px" }}>Cancel</Button>
+                  <Button variant="primary" onClick={sendata} style={{ marginLeft: "20%", width: "100px" }}>Approve</Button>
                 </Row>
-            </Card.Body>
-          </Card>
+              </Card.Body>
+            </Card>
           </div>
         )}
 
@@ -147,7 +147,7 @@ const SMMechanicalRepairs= props => {
 
             </tr>
           </thead>
-          
+
           <tbody>
             {mechanicalAppointment.map((appointment) => (
               <tr key={appointment._id} onClick={() => handleTableRowClick(appointment)}>
@@ -157,12 +157,12 @@ const SMMechanicalRepairs= props => {
                 <td>{appointment.appointmentdate ? `${appointment.appointmentdate.split('T')[0]} ${appointment.appointmenttime}` : ''}</td>
                 <td>{appointment.contactNo}</td>
                 <td>{appointment.issue}</td>
-                
+
                 <td>
-                  <Button variant="secondary" style={{marginLeft:"35%"}} onClick={() => handleMoreButtonClick(appointment)}>
+                  <Button variant="secondary" style={{ marginLeft: "35%" }} onClick={() => handleMoreButtonClick(appointment)}>
                     More
                   </Button></td>
-             
+
               </tr>
 
             ))}
