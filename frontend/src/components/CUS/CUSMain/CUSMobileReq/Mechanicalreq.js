3d7665be2e93de831a8cf6cd81a89f468e71d949
isiriuththara <isiri.uththara@gmail.com>
2024-04-04 16:13:21 +0530

    Modified Mechanicalreq file

--- frontend/src/components/CUS/CUSMain/CUSMobileReq/Mechanicalreq.js
+++ frontend/src/components/CUS/CUSMain/CUSMobileReq/Mechanicalreq.js
@@ -47,24 +47,21 @@ function Mechanicalreq() {
             </div>
             <div className="mobform-element">
               <label htmlFor="cusEmail" className='mobL1'>Email Address</label><br />
-              <input {...register("cusEmail", { 
-      required: true,
-      pattern: {
-        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // email validation
-        message: "Invalid email address"
-      }
-    })} className="small-input" type="text" id="cusEmail" placeholder="Enter Your Email" />
+              <input {...register("cusEmail", { required: true,pattern: {
+                                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // email validation
+                                                message: "Invalid email address"
+                                                }})} 
+              className="small-input" type="text" id="cusEmail" placeholder="Enter Your Email" />
               {errors.cusEmail && <span className="error">Email Address is required</span>}
             </div>
+
             <div className="mobform-element">
               <label htmlFor="vehicleNo" className='mobL1'>Vehicle Number</label><br />
-              <input {...register("vehicleNo", { 
-      required: true,
-      pattern: {
-        value: /^[A-Za-z0-9]{1,10}$/, //vehicle number validation
-        message: "Invalid vehicle number"
-      }
-    })} className="small-input" type="text" id="vehicleNo" placeholder="Enter Your Vehicle Number" />
+              <input {...register("vehicleNo", { required: true,pattern: {
+                                                value: /^[A-Za-z0-9]{1,10}$/, //vehicle number validation
+                                                message: "Invalid vehicle number"
+                                                } })} 
+              className="small-input" type="text" id="vehicleNo" placeholder="Enter Your Vehicle Number" />
               {errors.vehicleNo && <span className="error">Vehicle Number is required</span>}
             </div>
 
@@ -72,24 +69,20 @@ function Mechanicalreq() {
               <div className="mobyear-month-container">
                 <div className="mobyear-input">
                   <label htmlFor="reqDate" className='mobL1'>Date</label>
-                  <input {...register("reqDate", { 
-      required: true, 
-      pattern: {
-        value: /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{2}$/, //date format validation
-        message: "Invalid date format (DD/MM/YY)"
-      } 
-    })} className="small-input" type="text" id="reqDate" placeholder="DD/MM/YY" />
+                  <input {...register("reqDate", { required: true, pattern: {
+                                                value: /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{2}$/, //date format validation
+                                                message: "Invalid date format (DD/MM/YY)"
+                                                } })} 
+                  className="small-input" type="text" id="reqDate" placeholder="DD/MM/YY" />
                 </div>
                 <div className="mobmonth-input">
                   <label htmlFor="reqTime" className='mobL1'>Time</label>
-                  <input {...register("reqTime", { 
-      required: true, 
-      pattern: {
-        value: /^(?:[01]\d|2[0-3]):(?:[0-5]\d)$/, //time format validation
-        message: "Invalid time format (HH:MM)"
-      } 
-    })} className="small-input" type="text" id="reqTime" placeholder="Enter time" />
-     {errors.reqTime && <span className="error">{errors.reqTime.message}</span>}
+                  <input {...register("reqTime", { required: true, pattern: {
+                                                    value: /^(?:[01]\d|2[0-3]):(?:[0-5]\d)$/, //time format validation
+                                                    message: "Invalid time format (HH:MM)"
+                                                    } })} 
+                  className="small-input" type="text" id="reqTime" placeholder="Enter time" />
+                  {errors.reqTime && <span className="error">{errors.reqTime.message}</span>}
                 </div>
                 
               </div>
@@ -103,24 +96,23 @@ function Mechanicalreq() {
 
             <div className="mobform-element">
               <label htmlFor="issue" className='mobL1'>Issue</label><br />
-              <input className="small-input" type="text" id="issue" placeholder="Enter Vehicle Issue" />
+              <input {...register("issue")} className="small-input" type="text" id="issue" placeholder="Enter Vehicle Issue" />
             </div>
+
             <div className="mobform-element">
               <label htmlFor="contactNo" className='L1'>Contact Number</label><br />
-              <input  {...register("contactNo", { 
-      required: true,
-      pattern: {
-        value: /^[0-9]{10}$/, // Assuming a 10-digit phone number format
-        message: "Invalid contact number"
-      }
-    })} className="small-input" type="text" id="contactNo" placeholder="Enter Your Contact Number" />
-    {errors.contactNo && <span className="error">{errors.contactNo.message}</span>}
+              <input  {...register("contactNo", { required: true,pattern: {
+                                                value: /^[0-9]{10}$/, // Assuming a 10-digit phone number format
+                                                message: "Invalid contact number"
+                                                } })} 
+              className="small-input" type="text" id="contactNo" placeholder="Enter Your Contact Number" />
+            {errors.contactNo && <span className="error">{errors.contactNo.message}</span>}
          </div>
 
            
          <div className="mobcheckbox-container">
               <input type="checkbox" className="mobform-check-input" id="mobexampleCheck1" required />
-              <label className="mobform-check-label" htmlFor="mobexampleCheck1">By scheduling your appointment you agree to accept our terms and regulations</label><br /><br />
+              <label className="mobform-check-label" htmlFor="mobexampleCheck1">Accept the terms and conditions</label><br /><br />
             </div>
             <Button variant="primary" type="submit">Submit</Button>
 
