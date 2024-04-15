fabf290f4ff7dabcdee55d58ba0ed1409c6905f3
M.A.Rachith Tharana <rachcha2002@gmail.com>
2024-04-15 22:44:41 +0530

    Few Updates

--- frontend/src/components/CUS/CusHeader/Nav/Nav.js
+++ frontend/src/components/CUS/CusHeader/Nav/Nav.js
@@ -7,7 +7,6 @@ function Nav() {
   return (
     <nav className="cus-header-nav ms-auto">
       <ul className="d-flex align-items-center">
-        <CurrentDate />
         <NavAvatar />
       </ul>
     </nav>
--- frontend/src/components/CUS/CusHeader/Nav/NavAvatar.js
+++ frontend/src/components/CUS/CusHeader/Nav/NavAvatar.js
@@ -1,8 +1,8 @@
 import React, { useState, useContext } from "react";
-import cusavatar from '../../../../images/customerlogo.png'
-import { CusAuthContext } from "../../../../context/cus-authcontext"
-import { useNavigate,Link } from "react-router-dom";
-import { Modal, Button } from 'react-bootstrap';
+import cusavatar from "../../../../images/customerlogo.png";
+import { CusAuthContext } from "../../../../context/cus-authcontext";
+import { useNavigate, Link } from "react-router-dom";
+import { Modal, Button } from "react-bootstrap";
 
 function NavAvatar() {
   const [showLogoutModal, setShowLogoutModal] = useState(false);
@@ -18,119 +18,119 @@ function NavAvatar() {
 
   return (
     <div>
-    <li className="nav-item dropdown pe-3">
-      {cusauth.isLoggedIn && (
-      <a
-        className="nav-link nav-profile d-flex align-items-center pe-0"
-        href="#"
-        data-bs-toggle="dropdown"
-      >
-        <img src={cusavatar} alt="Profile" className="rounded-circle" />
-        <span className="d-none d-md-block dropdown-toggle ps-2">{cusauth.name}</span>
-      </a>
-      )}
-      <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow cus-profile">
-      {cusauth.isLoggedIn && (
-        <>
-         <li className="cus-dropdown-header">
-        <img src={cusavatar} alt="Profile" className="rounded-circle" />
-          <h6 style={{float:"center"}}>{cusauth.name}</h6>
-        </li>
-        <li>
-          <hr className="dropdown-divider" />
-        </li>
-
-        <li>
+      <li className="nav-item dropdown pe-3">
+        {cusauth.isLoggedIn && (
           <a
-            className="cus-dropdown-item d-flex align-items-center"
-            href="http://localhost:3000/customer/cusprofile"
-          >
-          
-            <span>My Profile</span>
-          </a>
-        </li>
-        <li>
-          <hr className="dropdown-divider" />
-        </li>
-
-        <li>
-          <a
-            className="cus-dropdown-item d-flex align-items-center"
-            href="users-profile.html"
-          >
-            
-            <span>My Payments</span>
-          </a>
-        </li>
-        <li>
-          <hr className="dropdown-divider" />
-        </li>
-
-        <li>
-          <a
-            className="cus-dropdown-item d-flex align-items-center"
-            href="http://localhost:3000/customer/appointment/myappointment"
-          >
-           
-            <span>My Appointments</span>
-          </a>
-        </li>
-        <li>
-          <hr className="dropdown-divider" />
-        </li>
-
-        <li>
-          <a className="cus-dropdown-item d-flex align-items-center" href="/">
-            
-            <span>Mobile Services</span>
-          </a>
-        </li>
-
-        <li>
-          <a
-            className="cus-dropdown-item d-flex align-items-center"
+            className="nav-link nav-profile d-flex align-items-center pe-0"
             href="#"
-            onClick={() => setShowLogoutModal(true)} // Show logout modal on click
+            data-bs-toggle="dropdown"
           >
-            
-            <span>Log Out</span>
+            <img src={cusavatar} alt="Profile" className="rounded-circle" />
+            <span className="d-none d-md-block dropdown-toggle ps-2">
+              {cusauth.name}
+            </span>
           </a>
-        </li>
-        </>
+        )}
+        <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow cus-profile">
+          {cusauth.isLoggedIn && (
+            <>
+              <li className="cus-dropdown-header">
+                <img src={cusavatar} alt="Profile" className="rounded-circle" />
+                <h6 style={{ float: "center" }}>{cusauth.name}</h6>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+
+              <li>
+                <a
+                  className="cus-dropdown-item d-flex align-items-center"
+                  href="http://localhost:3000/customer/cusprofile"
+                >
+                  <span>My Profile</span>
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+
+              <li>
+                <a
+                  className="cus-dropdown-item d-flex align-items-center"
+                  href="users-profile.html"
+                >
+                  <span>My Payments</span>
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+
+              <li>
+                <a
+                  className="cus-dropdown-item d-flex align-items-center"
+                  href="http://localhost:3000/customer/appointment/myappointment"
+                >
+                  <span>My Appointments</span>
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+
+              <li>
+                <a
+                  className="cus-dropdown-item d-flex align-items-center"
+                  href="#"
+                  onClick={() => setShowLogoutModal(true)} // Show logout modal on click
+                >
+                  <span>Log Out</span>
+                </a>
+              </li>
+            </>
+          )}
+        </ul>
+
+        {/* Logout confirmation modal */}
+        <Modal
+          show={showLogoutModal}
+          onHide={() => setShowLogoutModal(false)}
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Logout Confirmation</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Are you sure you want to logout?</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              onClick={() => setShowLogoutModal(false)}
+            >
+              No
+            </Button>
+            <Button variant="primary" onClick={handleLogout}>
+              Yes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </li>
+      {!cusauth.isLoggedIn && (
+        <div style={{ display: "flex" }}>
+          <Link to="/customer/cuslogin">
+            <Button variant="outline-light" style={{ margin: "8px" }}>
+              Signin{" "}
+            </Button>
+          </Link>
+          <Link to="/customer/cusreg">
+            <Button variant="outline-light" style={{ margin: "8px" }}>
+              Signup
+            </Button>
+          </Link>
+        </div>
       )}
-      </ul>
-
-      {/* Logout confirmation modal */}
-      <Modal show={showLogoutModal} onHide={() => setShowLogoutModal(false)} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Logout Confirmation</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Are you sure you want to logout?</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowLogoutModal(false)}>
-            No
-          </Button>
-          <Button variant="primary" onClick={handleLogout}>
-            Yes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      
-    </li>
-     {!cusauth.isLoggedIn && (
-      <div style={{display:"flex"}}>
-        <Link to="/customer/cuslogin">
-  <Button variant="outline-light">Signin </Button>
-  </Link>
-  <Link to="/customer/cusreg">
-  <Button variant="outline-light"style={{marginLeft:"1.5%"}}>Signup</Button>
-  </Link>
-  </div>
-  )}
-  </div>
-
+    </div>
   );
 }
 
