73f318494cab9280bb9fc0c5a2c109d0e98ce8b6
VinuriKariyawasam <vinurikariyawasam8@gmail.com>
2024-04-16 01:21:31 +0530

    updated supervisor dashboard
    added gm button and dashboard links

--- frontend/src/components/SUPER/SuperSidebar/SuperSideBar.js
+++ frontend/src/components/SUPER/SuperSidebar/SuperSideBar.js
@@ -2,14 +2,32 @@ import React from "react";
 import "./SuperSideBar.css";
 import supernavList from "../../../data/SUPER/supernavItem";
 import SuperNavItem from "./SuperNavItem";
+import { useContext } from "react";
+import { Button } from "react-bootstrap";
+import { BsArrowLeft } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { StaffAuthContext } from "../../../context/StaffAuthContext";
 
 function SuperSideBar() {
+  const { userPosition } = useContext(StaffAuthContext);
+  const navigate = useNavigate();
   return (
     <aside id="sidebar" className="sidebar">
       <ul className="sidebar-nav" id="sidebar-nav">
         {supernavList.map((nav) => (
           <SuperNavItem key={nav._id} nav={nav} />
         ))}
+        {userPosition === "General Manager" && (
+          <li>
+            <Button
+              variant="dark"
+              onClick={() => navigate("/staff/gm")}
+              style={{ margin: "10px" }}
+            >
+              <BsArrowLeft /> Gm Dashboard
+            </Button>
+          </li>
+        )}
       </ul>
     </aside>
   );
--- frontend/src/data/SUPER/supernavItem.js
+++ frontend/src/data/SUPER/supernavItem.js
@@ -26,11 +26,13 @@ const supernavList = [
       _id: 5,
       name: "Products",
       icon: "bi bi-card-checklist",
+      href: "/customer/products",
     },
     {
       _id: 6,
       name: "Schedule",
       icon: "bi bi-calendar3",
+      href: "/staff/sm/shedules",
     },
   ];
   
