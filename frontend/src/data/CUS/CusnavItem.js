e8c5738c9319ba08ded73cf02051d08ee9a7ea3d
M.A.Rachith Tharana <rachcha2002@gmail.com>
2024-04-16 15:41:26 +0530

    Update CusnavItem.js

--- frontend/src/data/CUS/CusnavItem.js
+++ frontend/src/data/CUS/CusnavItem.js
@@ -1,27 +1,25 @@
-import {useContext} from "react";
+import { useContext } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { CusAuthContext } from "../../context/cus-authcontext";
-import {CusAuthContext} from "../../context/cus-authcontext";
-import { useContext } from "react";
 
 function CusnavItem() {
   const CusAuth = useContext(CusAuthContext);
-  
- return (
+
+  return (
     <>
       <Navbar bg="#212121" data-bs-theme="dark">
         <Container>
           <Nav className="me-auto">
             <Nav.Link href="http://localhost:3000/customer">Home</Nav.Link>
             <NavDropdown title="Service" id="basic-nav-dropdown">
-            {CusAuth.isLoggedIn && (
-              <NavDropdown.Item href="http://localhost:3000/customer/products">
-                Product
-              </NavDropdown.Item>
-               )}
+              {CusAuth.isLoggedIn && (
+                <NavDropdown.Item href="http://localhost:3000/customer/products">
+                  Product
+                </NavDropdown.Item>
+              )}
               <NavDropdown.Item href="http://localhost:3000/customer/mobservices/mobilemain">
                 Mobile Services
               </NavDropdown.Item>
@@ -31,25 +29,24 @@ function CusnavItem() {
               <NavDropdown.Item>Our Services</NavDropdown.Item>
             </NavDropdown>
 
-            <Nav.Link href="http://localhost:3000/customer/contactus">Contact Us</Nav.Link>
-            <Nav.Link href="http://localhost:3000/customer/aboutus">About Us</Nav.Link>
+            <Nav.Link href="http://localhost:3000/customer/contactus">
+              Contact Us
+            </Nav.Link>
+            <Nav.Link href="http://localhost:3000/customer/aboutus">
+              About Us
+            </Nav.Link>
 
             <NavDropdown title="Customer Care" id="basic-nav-dropdown">
               {CusAuth.isLoggedIn && (
-              <NavDropdown.Item href="http://localhost:3000/customer/cusaffairs/consultation">
-                Consultation
+                <NavDropdown.Item href="http://localhost:3000/customer/cusaffairs/consultation">
+                  Consultation
+                </NavDropdown.Item>
+              )}
+              <NavDropdown.Item href="http://localhost:3000/customer/cusaffairs/allfeedback">
+                Feedback
               </NavDropdown.Item>
 
-               )}
-              <NavDropdown.Item href="http://localhost:3000/customer/cusaffairs/allfeedback">Feedback</NavDropdown.Item>
-
-          
-              <NavDropdown.Item href="http://localhost:3000/customer/payments/payonline">
-                My Payments
-
-              </NavDropdown.Item>
-
-              {cusauth.isLoggedIn ? (
+              {CusAuth.isLoggedIn ? (
                 <NavDropdown.Item href="http://localhost:3000/customer/payments/payonline">
                   My Payments
                 </NavDropdown.Item>
